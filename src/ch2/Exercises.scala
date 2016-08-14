7843b1781221101958e291224823d910dd196e5a
pldtc325 <pldtc325@gmail.com>
2016-08-14 21:52:44 +0800

    add ch2 Exercises

---
+++ src/ch2/Exercises.scala
@@ -0,0 +1,21 @@
+package functional_programming_in_scala.ch2
+
+/**
+  * Created by pltc325 on 16/8/5.
+  */
+object Exercises extends App {
+  def e_2_1(n:Int):Int = {
+    def fib(n:Int): Int = {
+      if(n == 0) 0
+      else if(n==1) 1
+      else fib(n-1) + fib(n-2)
+    }
+    fib(n)
+  }
+
+  println(e_2_1(0))
+  println(e_2_1(1))
+  println(e_2_1(2))
+  println(e_2_1(3))
+  println(e_2_1(4))
+}
